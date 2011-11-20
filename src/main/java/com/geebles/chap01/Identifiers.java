fec62dd7ad56f75b59c318e138c9b7d9e2234e4d
jonny s <jonny.sizlak@gmail.com>
2011-11-20 14:47:51 -0500

    adding identifiers

---
+++ src/main/java/com/geebles/chap01/Identifiers.java
@@ -0,0 +1,19 @@
+package com.geebles.chap01;
+
+public class Identifiers {
+
+	public static void main (String... args) {
+		int $dollars;
+		short ¥yen;
+		double £pound;
+		String €euro;
+		
+		String _underscore;
+		//String -dash;
+		
+		//String 7zip;
+		String zip7;
+		
+	}
+
+}
