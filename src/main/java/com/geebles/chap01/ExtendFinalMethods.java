d0b3920f922b6de2b75369c3476b8d1dedfd810c
jonny s <jonny.sizlak@gmail.com>
2011-11-30 07:36:52 -0500

    final methods

---
+++ src/main/java/com/geebles/chap01/ExtendFinalMethods.java
@@ -0,0 +1,1 @@
+package com.geebles.chap01;public class ExtendFinalMethods extends FinalMethods {	// No dice.	//	public void finalMethod() {	//	}	public void nonFinalMethod() {		System.out.println("With our powers combined!");	}}
---
+++ src/main/java/com/geebles/chap01/FinalMethods.java
@@ -0,0 +1,11 @@
+package com.geebles.chap01;
+
+public class FinalMethods {
+	public final void finalMethod() {
+		System.out.println("nyah nyah, you can't extend me!");
+	}
+	
+	public void nonFinalMethod() {
+		System.out.println("Extensorlate!");
+	}
+}
