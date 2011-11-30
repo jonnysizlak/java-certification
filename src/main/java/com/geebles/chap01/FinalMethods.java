0ce143c9496e2d6f14a02274f46300f0d4683969
jonny s <jonny.sizlak@gmail.com>
2011-11-30 07:41:23 -0500

    final parameters

--- src/main/java/com/geebles/chap01/FinalMethods.java
+++ src/main/java/com/geebles/chap01/FinalMethods.java
@@ -8,4 +8,21 @@ public class FinalMethods {
 	public void nonFinalMethod() {
 		System.out.println("Extensorlate!");
 	}
+	
+	public void finalParameters(int parameter, final int finalParameter, final Mutable mutant) {
+		parameter = 1;
+		// Can't assign final parameters.
+		// finalParameter = 1;
+		// Changing state via methods is cool
+		mutant.changeState();
+		// For a class to be truly final, it needs to have no methods on it that change state.
+		
+		// assigning to a new object entirely is not.
+		//mutant = new Mutable();
+	}
+	
+	private class Mutable {
+		public void changeState() {
+		}
+	}
 }
