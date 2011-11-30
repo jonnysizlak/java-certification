38ac68a6edb9caaed09a95ecccae5118916fd633
jonny s <jonny.sizlak@gmail.com>
2011-11-30 07:51:08 -0500

    more abstraction, testing to see what happens when a extended class changes it's implementation, but the extending classes aren't recompiled.

---
+++ src/main/java/com/geebles/chap01/AbstractChild.java
@@ -0,0 +1,8 @@
+package com.geebles.chap01;
+
+public class AbstractChild extends AbstractParent {
+	public static void main(String... args) {
+		AbstractChild instance = new AbstractChild();
+		instance.output();
+	}
+}
---
+++ src/main/java/com/geebles/chap01/AbstractParent.java
@@ -0,0 +1,12 @@
+package com.geebles.chap01;
+
+public abstract class AbstractParent {
+
+	// The extending classes do not copy the implementation out; they defer to the class.
+	// So if you extend a class, and that class' implementation is updated, you'll use
+	// the updated implementation.
+	public void output() {
+		// System.out.println("Orignal implementation.");
+		System.out.println("Updated implementation.");
+	}
+}
