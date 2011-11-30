120a314aa2f448d0e5be9600cd2c0bc85609e619
jonny s <jonny.sizlak@gmail.com>
2011-11-29 19:43:51 -0500

    local variables

---
+++ src/main/java/com/geebles/chap01/LocalVariables.java
@@ -0,0 +1,24 @@
+package com.geebles.chap01;
+
+public class LocalVariables {
+
+	public void method() {
+		// local variables can be final
+		final int f = 1;
+		
+		// local variables cannot be abstract
+		//abstract int a = 1;
+		
+		// local variables cannot be private
+		// private int pr = 1;
+		// local variables cannot be public
+		// public int pu = 1;
+		// local variables cannot be private
+		// protected int pr = 1;
+		// local variables cannot be static
+		//static int s = 1;
+		// local variables cannot be strictfp
+		//strictfp int sfp = 1;
+	}
+
+}
