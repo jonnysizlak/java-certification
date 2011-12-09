035824455549b002ae4f22e78bdbfd63d17e8261
jonny s <jonny.sizlak@gmail.com>
2011-12-09 08:18:45 -0500

    overloading and overriding

--- src/main/java/com/geebles/chap02/Polymorphism.java
+++ src/main/java/com/geebles/chap02/Polymorphism.java
@@ -5,11 +5,17 @@ import java.util.ArrayList;
 
 public class Polymorphism {
 	
-	public void polymorphize() {
+	public void polymorphize() throws Exception {
 		System.out.println("..without form!");
 	}
+
+	public void polymorphize(String form) throws Exception {
+		System.out.println(form);
+	}
 	
-	public static void main (String... args) {
+	protected static int whatnow = 7;
+	
+	public static void main (String... args) throws Exception {
 		List<Polymorphism> polys = new ArrayList<Polymorphism>();
 		polys.add(new Polymorphism());
 		polys.add(new Fire());
@@ -21,13 +27,53 @@ public class Polymorphism {
 		for(Polymorphism poly : polys) {
 			poly.polymorphize();
 		}
+		
+		Polymorphism p1 = new Polymorphism();
+		p1.polymorphize("Cheese");
+		
+		Polymorphism p2 = new Water();
+		p2.polymorphize("Cheese");
+		
+		new Polymorphism().poly(p1);
+		new Polymorphism().poly(p2);
+		
+		if (p2 instanceof Water) {
+			Water w = (Water) p2;
+			w.getWet();
+		}
+		
+		// Inheritance means jack to things that are static.
+		System.out.println(Polymorphism.whatnow + " " + Water.whatnow);
+	}
+	
+	public void poly(Polymorphism p) throws Exception {
+		p.polymorphize();
+		p.polymorphize("Cheese");
+	}
+	
+	
+	public void poly(Water w) {
+		w.polymorphize();
+		w.polymorphize("Cheez-whiz");
 	}
 }
 
 class Water extends Polymorphism {
+	// Subclasses don't need to throw exceptions just because the method they're overriding does.
 	public void polymorphize() {
 		System.out.println("Water!");
 	}
+	
+	public void polymorphize(String form) {
+		System.out.println(form + "!");
+	}
+
+	public void getWet() {
+		
+	}
+	
+	// Less restrictive is okay
+	public static int whatnow = 8;
 
 }
 
