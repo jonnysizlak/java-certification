705204d21441a8ac994afa3c5cf29e840766a986
jonny s <jonny.sizlak@gmail.com>
2011-12-12 21:03:24 -0500

    constructors.

---
+++ src/main/java/com/geebles/chap02/ConstructorsAndInstantiation.java
@@ -0,0 +1,14 @@
+package com.geebles.chap02;
+
+public class ConstructorsAndInstantiation {
+	public ConstructorsAndInstantiation(int arg1) {
+		System.out.println(trouble);
+	}
+	
+	public int trouble = 7;
+	
+	public static void main (String... args) {
+		ConstructorsAndInstantiation cai = new ConstructorsAndInstantiation(1);
+		System.err.println(cai.trouble);
+	}
+}
