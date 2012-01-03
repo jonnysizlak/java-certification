7554b89758c2ef375073b604c0e3fb14c69aee5d
jonny s <jonny.sizlak@gmail.com>
2012-01-03 10:21:35 -0500

    more operators

--- src/main/java/com/geebles/javacert/chap04/Operators.java
+++ src/main/java/com/geebles/javacert/chap04/Operators.java
@@ -1,5 +1,8 @@
 package com.geebles.javacert.chap04;
 
+import java.lang.StringBuilder;
+import java.lang.StringBuffer;
+
 public class Operators {
 	public static void main (String... args) {
 		
@@ -55,7 +58,35 @@ public class Operators {
 		System.out.println("o1 == o2 : " + (o1 == o2));
 		System.out.println("o1 == o3 : " + (o1 == o3));
 		
-		
+		System.out.println("-----------");
+		Color c1 = Color.RED;
+		Color c2 = Color.RED;
+		Color c3 = Color.GREEN;
+		Color c4 = Color.BLUE;
 
+		System.out.println("c1 == c1 : " + (c1 == c1));
+		System.out.println("c1 == c2 : " + (c1 == c2));
+		System.out.println("c1 == c3 : " + (c1 == c3));
+		System.out.println("c1 == c4 : " + (c1 == c4));
+
+		System.out.println("c1.equals(c1) : " + (c1.equals(c1)));
+		System.out.println("c1.equals(c2) : " + (c1.equals(c2)));
+		System.out.println("c1.equals(c3) : " + (c1.equals(c3)));
+		System.out.println("c1.equals(c4) : " + (c1.equals(c4)));
+
+		System.out.println("-----------");
+
+		Object string = "";
+		Object buffer = new StringBuffer();
+		Object builder = new StringBuilder();
+		
+		System.out.println("string instanceof String: " + (string instanceof String));
+		System.out.println("buffer instanceof String : " + (buffer instanceof String));
+		System.out.println("builder instanceof String : " + (builder instanceof String));
+		System.out.println("string instanceof Object : " + (string instanceof Object));
+		
 	}
+	
+	enum Color { RED, GREEN, BLUE } // the semicolor is optional after an enum declaration
+	
 }
