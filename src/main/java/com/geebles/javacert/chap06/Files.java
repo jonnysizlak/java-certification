a1377f3bdfeabfa20d23430937464b785594cdea
jonny sizlak <jonny.sizlak@gmail.com>
2012-01-29 13:11:22 -0500

    creating files

--- src/main/java/com/geebles/javacert/chap06/Files.java
+++ src/main/java/com/geebles/javacert/chap06/Files.java
@@ -4,6 +4,24 @@ import java.io.*;
 
 public class Files {
 	public static void main (String... args) {
-		File file1 = new File("afile.txt");
+		Files f = new Files();
+		f.file1();
+
+	}
+
+	public void file2() {
+	
+	}
+	
+	
+	public void file1() {
+		try {
+			File file1 = new File("afile.txt");
+			System.out.println(file1.exists());
+			System.out.println(file1.createNewFile());
+			System.out.println(file1.exists());
+		} catch (IOException ioe) {
+		
+		}
 	}
 }
