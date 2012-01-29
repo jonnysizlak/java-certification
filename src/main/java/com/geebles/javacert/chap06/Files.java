20ea7c88c42acf373fac5b3e4ae0dd2ca5458165
jonny sizlak <jonny.sizlak@gmail.com>
2012-01-29 13:03:22 -0500

    files.

---
+++ src/main/java/com/geebles/javacert/chap06/Files.java
@@ -0,0 +1,9 @@
+package com.geebles.javacert.chap06;
+
+import java.io.*;
+
+public class Files {
+	public static void main (String... args) {
+		File file1 = new File("afile.txt");
+	}
+}
--- src/main/java/com/geebles/javacert/chap06/Strings.java
+++ src/main/java/com/geebles/javacert/chap06/Strings.java
@@ -55,4 +55,5 @@ public class Strings {
 		buffer.reverse();
 		
 	}
+
 }
