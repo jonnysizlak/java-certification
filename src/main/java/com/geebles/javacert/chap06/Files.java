500a2df72ad6a88a44f1236ee18aef537a8c2626
jonny sizlak <jonny.sizlak@gmail.com>
2012-01-29 13:36:14 -0500

    writing and reading from files

--- src/main/java/com/geebles/javacert/chap06/Files.java
+++ src/main/java/com/geebles/javacert/chap06/Files.java
@@ -6,11 +6,29 @@ public class Files {
 	public static void main (String... args) {
 		Files f = new Files();
 		f.file1();
-
+		f.file2();
 	}
 
 	public void file2() {
-		
+		try {
+			File file = new File("afile.txt");
+			file.createNewFile();
+			FileWriter writer = new FileWriter(file);
+			writer.write("hello world\n");
+			writer.flush();
+			writer.close();
+			
+			char[] in = new char[50];
+			
+			FileReader reader = new FileReader(file);
+			int size = reader.read(in);
+			for(int i = 0; i < size; ++i) {
+				System.out.print(in[i]);
+			}
+			reader.close();
+			file.delete();
+		} catch (IOException ioe) { 
+		}
 	}
 	
 	
