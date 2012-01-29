524d97b06ee1cb92dc052cf64e1d773bdf61e388
jonny sizlak <jonny.sizlak@gmail.com>
2012-01-29 14:32:22 -0500

    adding and removing directories

--- src/main/java/com/geebles/javacert/chap06/Files.java
+++ src/main/java/com/geebles/javacert/chap06/Files.java
@@ -7,8 +7,55 @@ public class Files {
 		Files f = new Files();
 		f.file1();
 		f.file2();
+		f.directory();
+		f.directory2();
 	}
 
+	public void directory2() {
+		try {
+			File deldir = new File("deldir");
+			deldir.mkdir();
+			File delfile1 = new File(deldir, "delfile1.txt");
+			delfile1.createNewFile();
+			
+			File delfile2 = new File(deldir, "delfile2.txt");
+			delfile2.createNewFile();
+			
+			delfile1.delete();
+			System.out.println("deldir is " + deldir.delete());
+			
+			File newname = new File(deldir, "newName.txt");
+			delfile2.renameTo(newname);
+			
+			File newdir = new File("newdir");
+			deldir.renameTo(newdir);
+
+			System.out.println("====================");
+			for(String current : newdir.list()) {
+				System.out.println(current);
+			}
+			System.out.println("====================");
+
+			delfile2.delete();
+			deldir.delete();
+			
+		} catch (IOException ioe) {
+		}
+	}
+	
+	public void directory() {
+		try {
+			File dir = new File("temp");
+			dir.mkdir();
+			dir.delete();
+			File file = new File(dir, "afile.txt");
+			file.createNewFile();
+			file.delete();
+			System.out.println(dir.delete());
+		} catch (IOException ioe) {
+		}
+	}
+	
 	public void file2() {
 		try {
 			File file = new File("afile.txt");
