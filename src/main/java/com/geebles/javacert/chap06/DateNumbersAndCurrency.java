8621621277541622ffd4e9d058c5f7bf1a2aa2c0
jonny s <jonny.sizlak@gmail.com>
2012-02-01 08:50:52 -0500

    next up: dates, numbers, and currency

---
+++ src/main/java/com/geebles/javacert/chap06/DateNumbersAndCurrency.java
@@ -0,0 +1,7 @@
+package com.geebles.javacert.chap06;
+
+public class DateNumbersAndCurrency {
+	public static void main(String... args) {
+		You stopped on page 473: Dates, Numbers, and Currency
+	}
+}
